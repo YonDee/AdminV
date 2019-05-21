93c82b5e12f28cb01308d47af1f8b4a6eadcf49d
Bykaii <young@deelol.com>
2019-05-21 14:37:05 +0800

    feat: added clear layout

---
+++ layouts/clear.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <Nuxt />
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
