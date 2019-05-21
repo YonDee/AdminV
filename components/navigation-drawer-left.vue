bec9cad92ce4b9944d5596a7f2a20d0e0b3aab1e
Bykaii <young@deelol.com>
2019-05-21 11:27:42 +0800

    refactor: split components

---
+++ components/navigation-drawer-left.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-flex>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click.stop="left = !left">
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-navigation-drawer
+      v-model="left"
+      temporary
+      fixed
+    ></v-navigation-drawer>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      drawer: this.mainVisible,
+      left: this.childVisible
+    }
+  },
+  props: {
+    mainVisible: {
+      type: Boolean,
+      default: false
+    },
+    childVisible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  watch: {
+    mainVisible(val){
+      this.drawer = val
+    },
+    childVisible(val){
+      this.left = val
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -12,27 +12,9 @@
       <v-spacer></v-spacer>
       <v-toolbar-side-icon @click.stop="drawerRight = !drawerRight"></v-toolbar-side-icon>
     </v-toolbar>
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-list dense>
-        <v-list-tile @click.stop="left = !left">
-          <v-list-tile-action>
-            <v-icon>exit_to_app</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-navigation-drawer
-      v-model="left"
-      temporary
-      fixed
-    ></v-navigation-drawer>
+    <leftDrawer
+     :mainVisible="drawer"
+     :childVisible="left"/>
     <v-content>
       <v-container fluid fill-height>
         <v-layout justify-center align-center>
@@ -55,9 +37,11 @@
 
 <script>
 import rightDrawer from '~/components/navigation-drawer-right'
+import leftDrawer from '~/components/navigation-drawer-left'
 export default {
   components: {
-    rightDrawer
+    rightDrawer,
+    leftDrawer
   },
   data: () => ({
     drawer: false,
