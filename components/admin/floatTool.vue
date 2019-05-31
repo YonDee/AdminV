9d325e184f07719b4fb37e405c967ddc9e6206df
Bykaii <young@deelol.com>
2019-05-31 16:05:57 +0800

    feat: created base element to 'floatTool' component

--- components/admin/floatTool.vue
+++ components/admin/floatTool.vue
@@ -1,14 +1,26 @@
 <template>
-<div class="float-tool animated zoomIn fast">
-  <v-btn fab dark color="primary">
-    <v-icon dark>add</v-icon>
+<div :class="['float-tool ', visible ? 'clicked' :  '']" >
+  <v-btn fab dark small color="primary" v-for="(iten,i) in 3" :key="i" :class="['btn-tool ', 'position-' + ( i+1 )]" v-show="visible && i<3" >
+    <v-icon dark>remove</v-icon>
+  </v-btn>
+  <v-btn fab dark :color="visible ? 'grey' : 'primary'" @click="showTools()" class="animated zoomIn fast">
+    <v-icon dark>clear</v-icon>
   </v-btn>
 </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    showTools(){
+      this.visible = !this.visible
+    },
+  }
 }
 </script>
 
@@ -17,4 +29,21 @@ export default {
   position fixed
   bottom 45px
   right 10px
+  transition all .3s ease
+  transform rotate(-45deg)
+  .btn-tool
+    position absolute
+    transition all .3s ease
+    &.position-1
+      left -70px
+      top 7px
+    &.position-2
+      left -45px
+      top -45px
+    &.position-3
+      left 7px
+      top -70px
+  &.clicked
+    transform rotate(0)
+    transition all .3s ease
 </style>
