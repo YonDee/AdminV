472e06c6ba315970f942ac821201b81e2fe59ea8
YonDee <im.yondee@gmail.com>
2019-06-18 11:53:02 +0800

    fix: 'components/admin/navigation-drawer-right'

--- components/admin/navigation-drawer-right.vue
+++ components/admin/navigation-drawer-right.vue
@@ -39,9 +39,9 @@
       >
         <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar">
       </v-avatar>
-      <h2>{{ $store.state.user.data.name }}</h2>
-      <p>Email: {{ $store.state.user.data.email }}</p>
-      <p>account: {{ $store.state.user.data.account }}</p>
+      <h2>{{ $store.state.user.data.name || '' }}</h2>
+      <p>Email: {{ $store.state.user.data.email || '' }}</p>
+      <p>account: {{ $store.state.user.data.account ||  ''}}</p>
     </div>
   </v-navigation-drawer>
 </v-flex>
